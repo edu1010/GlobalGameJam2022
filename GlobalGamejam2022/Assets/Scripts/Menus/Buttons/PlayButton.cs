e2016fe18646b79ee5d7509b779cf90551b4e2dd
edu1010 <39130123+edu1010@users.noreply.github.com>
2022-01-28 12:55:22 +0100

    menus

---
+++ GlobalGamejam2022/Assets/Scripts/Menus/Buttons/PlayButton.cs
@@ -0,0 +1,11 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.EventSystems;
+public class PlayButton : MonoBehaviour, IPointerClickHandler
+{
+    public void OnPointerClick(PointerEventData eventData)
+    {
+        throw new System.NotImplementedException();
+    }
+}
