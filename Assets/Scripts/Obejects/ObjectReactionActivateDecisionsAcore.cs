074c5bb8b5d796d78df6e23690391eb22bd78ce7
edu1010 <39130123+edu1010@users.noreply.github.com>
2022-01-30 09:41:30 +0100

    activarAncoreCogiendoObjeto

---
+++ Assets/Scripts/Obejects/ObjectReactionActivateDecisionsAcore.cs
@@ -0,0 +1,66 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ObjectReactionActivateDecisionsAcore : MonoBehaviour, IterfaceInteractable
+{
+    private Material _mat;
+    Color m_originalColor;
+    bool m_apeal = false;
+    float timer = 0f;
+    float timerColor = 0f;
+    GameObject gameObject;
+    // Start is called before the first frame update
+    void Start()
+    {
+        Renderer nRend = GetComponent<Renderer>();
+        _mat = nRend.material;
+        m_originalColor = _mat.GetColor("_BaseColor");
+        gameObject = GameObject.FindWithTag("ancore");
+    }
+
+
+    private void Update()
+    {
+        if (m_apeal)
+        {
+            if (Time.time - timer > 0.5f)
+            {
+                m_apeal = false;
+            }
+            timerColor += Time.deltaTime;
+            if (timerColor > 1.25f)
+            {
+                ChangeColor(Color.black);
+                if (timerColor > 1.25 * 2f)
+                {
+                    ChangeColor(m_originalColor);
+                    timerColor = 0;
+                }
+            }
+        }
+        else
+            ChangeColor(m_originalColor);
+
+    }
+    public void AppealInteractuable()
+    {
+        m_apeal = true;
+        timer = Time.time;
+
+
+    }
+
+    public void Interact()
+    {
+        ChangeColor(m_originalColor);
+        //GameController.GetGameController().GetPlayer().StartObservingObject(gameObject);
+        gameObject.SetActive(true);
+    }
+    public void ChangeColor(Color color)
+    {
+        Color nNew = Color.black;//do whatever you want here
+        _mat.SetColor("_BaseColor", color);
+    }
+
+}
