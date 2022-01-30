a84751819c828ae5599fcd06a0b042a68befaf52
Veronika Bolshakova <veronikabox@gmail.com>
2022-01-30 10:21:31 +0100

    Music and sounds

--- Assets/Scripts/MusicPlayer.cs
+++ Assets/Scripts/MusicPlayer.cs
@@ -2,11 +2,8 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
-public class MusicController : MonoBehaviour
+public class MusicPlayer : MonoBehaviour
 {
-    AudioSource m_audioSurce;
-    
-    public List<AudioClip> songs;
     // Start is called before the first frame update
     void Start()
     {
