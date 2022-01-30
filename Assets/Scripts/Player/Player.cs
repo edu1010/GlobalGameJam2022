abfdc16bd7cdc30c9dff7b056edf5932011b2a0e
edu1010 <39130123+edu1010@users.noreply.github.com>
2022-01-30 03:10:20 +0100

    subida pruebas

--- Assets/Scripts/Player/Player.cs
+++ Assets/Scripts/Player/Player.cs
@@ -44,7 +44,7 @@ public class Player : MonoBehaviour
     // Start is called before the first frame update
     void Start()
     {
-        
+        GameController.GetGameController().SetPlayer(this);
     }
 
     // Update is called once per frame
