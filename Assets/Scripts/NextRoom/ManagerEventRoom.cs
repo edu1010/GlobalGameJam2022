42f353056e681b98b7aa7551255fad188f47b3ad
edu1010 <39130123+edu1010@users.noreply.github.com>
2022-01-30 05:24:23 +0100

    Mas de una variable scriptableObjkect

--- Assets/Scripts/NextRoom/ManagerEventRoom.cs
+++ Assets/Scripts/NextRoom/ManagerEventRoom.cs
@@ -8,6 +8,10 @@ public class ManagerEventRoom : MonoBehaviour
     public void TakeDecision()
     {
         LevelsData.GetLevelsData().IncrementVariable(m_Event.m_VarToChange.ToString(), m_Event.m_Quantity);
+
+        LevelsData.GetLevelsData().IncrementVariable(m_Event.m_VarToChange2.ToString(), m_Event.m_Quantity2);
+        LevelsData.GetLevelsData().IncrementVariable(m_Event.m_VarToChange3.ToString(), m_Event.m_Quantity3);
+
         LevelsData.GetLevelsData().CalculateNextScene(m_Event.m_CurrentEtapa);
     }
 }
